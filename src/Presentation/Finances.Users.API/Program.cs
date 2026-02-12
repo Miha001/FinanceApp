124eecb90cf98febb3d57ee477915440413a21db
Mikhail Zeykan <zeykan_12@mail.ru>
2026-02-12 17:07:28 +0300

    Remove .AddControllers()

--- src/Presentation/Finances.Users.API/Program.cs
+++ src/Presentation/Finances.Users.API/Program.cs
@@ -17,7 +17,6 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
